e58371e6a8b36ccf02d1307f2211b63f3d9f3b5c
Robert Talamantez <robert.m.talamantez@gmail.com>
2024-10-24 00:12:35 -0700

    wip - debug version

--- main.ts
+++ main.ts
@@ -8,7 +8,5 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
-import dev from "$fresh/dev.ts";
 import "jsr:@std/dotenv/load";
-await dev(import.meta.url, "./main.ts");
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
